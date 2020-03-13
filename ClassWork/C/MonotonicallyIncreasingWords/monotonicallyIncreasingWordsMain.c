5b18df0814aa77ad0549e3d1e97899952c4a212a
AJ Ferraro <60527694+HHAJ98@users.noreply.github.com>
2020-03-12 17:10:59 -0700

    Update monotonicallyIncreasingWordsMain.c

--- ClassWork/C/MonotonicallyIncreasingWords/monotonicallyIncreasingWordsMain.c
+++ ClassWork/C/MonotonicallyIncreasingWords/monotonicallyIncreasingWordsMain.c
@@ -1,3 +1,17 @@
+/**
+ * A program that will read a text file or take input from the command line and
+ * print out each monotonically increasing word that it scans. A few assumptions
+ * are made about the word, namely that words:
+ *	- are all lowercase
+ *	- are less than 100 characters long
+ *	- contain no special symbols  
+ *
+ * @author:     AJ Ferraro
+ * Course:      COMP B13
+ * Created on:  03.12.2020
+ * Source File: monotonicallyIncreasingWordsMain.c
+ */
+
 #include <stdio.h>
 
 int monotonicallyIncreasingWord(char*);
