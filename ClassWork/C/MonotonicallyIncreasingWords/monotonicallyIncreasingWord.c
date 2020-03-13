25d859e82734fe82c372439a7343948a13fa9b00
AJ Ferraro <60527694+HHAJ98@users.noreply.github.com>
2020-03-12 17:02:19 -0700

    Update the comments on monotonicallyIncreasingWord.c
    Added File and function comments.

--- ClassWork/C/MonotonicallyIncreasingWords/monotonicallyIncreasingWord.c
+++ ClassWork/C/MonotonicallyIncreasingWords/monotonicallyIncreasingWord.c
@@ -1,3 +1,24 @@
+/**
+ * This file contains the fucntion monotonicallyIncreasingWord
+ *
+ * @author:     AJ Ferraro
+ * Course:      COMP B13
+ * Created on:  03.12.2020
+ * Source File: monotonicallyIncreasingWord.c
+ */
+
+
+/**
+  * This function returns 0 if a word isn't monotonically Increasing, and 1 
+  * otherwise. That is: if each letter in the word is strictly alphabeticaly
+  * after the letter before it. This is a simple check, and assumes that words:
+  * 	- are all lowercase
+  *		- are less than 100 characters long
+  *		- contain no special symbols
+  *  
+  * @param *word a pointer to a c string < 100 characters long including NULL
+  * @return 1 if the word is monotonically increasing, 0 if otherwise. 
+  */ 
 int monotonicallyIncreasingWord(char *word){
 	char *letter;
 	for(letter = word +1; *letter; letter++){
