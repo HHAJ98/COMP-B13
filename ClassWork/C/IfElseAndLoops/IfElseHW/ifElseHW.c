36b1ade5392b0cf7c802fcde48a1627058cf13bd
AJ Ferraro <AJFerraroProfessional@gmail.com>
2020-03-24 21:13:44 -0700

    Add files via upload

--- ClassWork/C/IfElseAndLoops/IfElseHW/ifElseHW.c
+++ ClassWork/C/IfElseAndLoops/IfElseHW/ifElseHW.c
@@ -1,25 +1,37 @@
+/**
+ * Reverse-engineered from a functionally identical assembly language program
+ * of the same name. Intended to be structured in exactly the same way, this
+ * program takes a number from the command line and prints out another number
+ * based on the input
+ *
+ * @author:     AJ Ferraro
+ * Course:      COMP B13
+ * Created on:  03.24.2020
+ * Source File: ifElseHW.c
+ */
+
 #include <stdio.h> 
 #include <stdlib.h>
 
 int main (int argc, char *argv[]){
-    int x = atoi(argv[1]);
-    int y;
-    if(x == 4){
-        y = x + 20;
-    } else if (x <= 6 || x > 9){
-        if(x != 0){
-            if(x != -1){
-                y = 99;
+    int input = atoi(argv[1]);
+    int output;
+    if(input == 4){
+        output = input + 20;
+    } else if (input <= 6 || input > 9){
+        if(input != 0){
+            if(input != -1){
+                output = 99;
             } else {
-                y = -44;
+                output = -44;
             }
         } else {
-            y = 44;
+            output = 44;
         }
     } else {
-        y = x + 20;
+        output = input + 20;
     }
-    printf("%d\n", y);
+    printf("%d\n", output);
 
     return 0;
 }
