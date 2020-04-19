73659d000cd6e1546f6bf5c9d8596199463327b7
HHAJ98 <ajferraroprofessional@gmail.com>
2020-04-19 16:21:33 -0700

    whitespace

--- ClassWork/x64_GAS/MyName/myName.s
+++ ClassWork/x64_GAS/MyName/myName.s
@@ -25,12 +25,12 @@ LoopBody:
 	imull	%edx,	%ecx		# NumRows * NumCols
 
 # The start of the A
-	cmpl	$1,		%eax
+	cmpl	$1, 	%eax
 	jle		A0
 	movb	$'*',	1(%rdi, %rcx)
 	movb	$'*',	13(%rdi, %rcx)
 A0:
-	cmpl	$0,		%eax
+	cmpl	$0, 	%eax
 	jle		A1
 	movb	$'*',	3(%rdi, %rcx)
 	movb	$'*',	11(%rdi, %rcx)
@@ -38,32 +38,32 @@ A1:
 	movb	$'*',	5(%rdi, %rcx)
 	movb	$'*',	9(%rdi, %rcx)
 
-	cmpl	$7,		%eax
+	cmpl	$7, 	%eax
 	jge		J0
-	cmpl	$3,		%eax
+	cmpl	$3, 	%eax
 	je		J0
 	cmpl	$4, 	%eax
 	je		J0
 	movb	$'*',	7(%rdi, %rcx)
 # The start of the J
 J0:
-	cmpl	$3,		%eax
+	cmpl	$3, 	%eax
 	jle		J1
 	cmpl	$7, 	%eax
 	jge		J1
 	movb	$'*',	17(%rdi, %rcx)
 J1:
-	cmpl	$3,		%eax
+	cmpl	$3, 	%eax
 	jle		J2
-	cmpl	$8,		%eax
+	cmpl	$8, 	%eax
 	jge		J2
 	movb	$'*',	19(%rdi, %rcx)
 J2:
-	cmpl	$3,		%eax
+	cmpl	$3, 	%eax
 	jle		J3
 	movb	$'*',	21(%rdi, %rcx)
 J3:
-	cmpl	$5,		%eax
+	cmpl	$5, 	%eax
 	jle		J4
 	movb	$'*',	23(%rdi, %rcx)
 J4:
@@ -73,7 +73,7 @@ J4:
 	jge		J5
 	movb	$'*',	27(%rdi, %rcx)
 J5:
-	cmpl	$7,		%eax
+	cmpl	$7, 	%eax
 	jge		F0
 	movb	$'*',	29(%rdi, %rcx)
 # The start of the F
@@ -82,15 +82,15 @@ F0:
 	movb	$'*',	39(%rdi, %rcx)
 	movb	$'*',	41(%rdi, %rcx)
 
-	cmpl	$6,		%eax
+	cmpl	$6, 	%eax
 	jge		F1
-	cmpl	$3,		%eax
+	cmpl	$3, 	%eax
 	je		F1
 	movb	$'*',	43(%rdi, %rcx)
 	movb	$'*',	45(%rdi, %rcx)
 	movb	$'*',	47(%rdi, %rcx)
 F1:
-	cmpl	$3,		%eax
+	cmpl	$3, 	%eax
 	jge		F2
 	movb	$'*',	49(%rdi, %rcx)
 F2:
