7286930dec1b3d23040e105b839a7d9c8b55cbc2
HHAJ98 <ajferraroprofessional@gmail.com>
2020-04-19 16:16:23 -0700

    Remove unessecary instructions and register usage

--- ClassWork/x64_GAS/MyName/myName.s
+++ ClassWork/x64_GAS/MyName/myName.s
@@ -22,16 +22,15 @@ myName:
 	jmp	Condition
 LoopBody:
 	movl	%eax,	%ecx		# Get current row
-	movq	%rcx,	%r8			# Move current row to %r8
 	imull	%edx,	%ecx		# NumRows * NumCols
 
 # The start of the A
-	cmpq	$1,		%r8
+	cmpl	$1,		%eax
 	jle		A0
 	movb	$'*',	1(%rdi, %rcx)
 	movb	$'*',	13(%rdi, %rcx)
 A0:
-	cmpq	$0,		%r8
+	cmpl	$0,		%eax
 	jle		A1
 	movb	$'*',	3(%rdi, %rcx)
 	movb	$'*',	11(%rdi, %rcx)
@@ -39,42 +38,42 @@ A1:
 	movb	$'*',	5(%rdi, %rcx)
 	movb	$'*',	9(%rdi, %rcx)
 
-	cmpq	$7,		%r8
+	cmpl	$7,		%eax
 	jge		J0
-	cmpq	$3,		%r8
+	cmpl	$3,		%eax
 	je		J0
-	cmpq	$4, 	%r8
+	cmpl	$4, 	%eax
 	je		J0
 	movb	$'*',	7(%rdi, %rcx)
 # The start of the J
 J0:
-	cmpq	$3,		%r8
+	cmpl	$3,		%eax
 	jle		J1
-	cmpq	$7, 	%r8
+	cmpl	$7, 	%eax
 	jge		J1
 	movb	$'*',	17(%rdi, %rcx)
 J1:
-	cmpq	$3,		%r8
+	cmpl	$3,		%eax
 	jle		J2
-	cmpq	$8,		%r8
+	cmpl	$8,		%eax
 	jge		J2
 	movb	$'*',	19(%rdi, %rcx)
 J2:
-	cmpq	$3,		%r8
+	cmpl	$3,		%eax
 	jle		J3
 	movb	$'*',	21(%rdi, %rcx)
 J3:
-	cmpq	$5,		%r8
+	cmpl	$5,		%eax
 	jle		J4
 	movb	$'*',	23(%rdi, %rcx)
 J4:
 	movb	$'*',	25(%rdi, %rcx)
 	
-	cmpq	$8,		%r8
+	cmpl	$8,		%eax
 	jge		J5
 	movb	$'*',	27(%rdi, %rcx)
 J5:
-	cmpq	$7,		%r8
+	cmpl	$7,		%eax
 	jge		F0
 	movb	$'*',	29(%rdi, %rcx)
 # The start of the F
@@ -83,15 +82,15 @@ F0:
 	movb	$'*',	39(%rdi, %rcx)
 	movb	$'*',	41(%rdi, %rcx)
 
-	cmpq	$6,		%r8
+	cmpl	$6,		%eax
 	jge		F1
-	cmpq	$3,		%r8
+	cmpl	$3,		%eax
 	je		F1
 	movb	$'*',	43(%rdi, %rcx)
 	movb	$'*',	45(%rdi, %rcx)
 	movb	$'*',	47(%rdi, %rcx)
 F1:
-	cmpq	$3,		%r8
+	cmpl	$3,		%eax
 	jge		F2
 	movb	$'*',	49(%rdi, %rcx)
 F2:
@@ -99,6 +98,4 @@ F2:
 Condition:
 	cmpl	%esi,	%eax	# Jump if we've done this to all the rows.
 	jl	LoopBody
-
-	# movq	$0,	%rax
 ret
